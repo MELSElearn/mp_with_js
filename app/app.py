cef76acab7d7da608d649ddb1b577a34a67e2348
MELSElearn <78197595+MELSElearn@users.noreply.github.com>
2022-03-29 18:13:57 +0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -12,11 +12,4 @@ def hello_world():
     if request_type_str == 'GET':
         return render_template('index.html', href2='')
     else:
-        myage = request.form['age']
-        mygender = request.form['gender']
-        model = load('app/bread-recommender.joblib')
-        np_arr = np.array([myage, mygender])
-        predictions = model.predict([np_arr])  
-        predictions_to_str = str(predictions)
-        #return predictions_to_str
-        return render_template('index.html', href2='The suitable bread for you (age:'+str(myage)+' ,gender:'+str(mygender)+') is:'+predictions_to_str)
+        return render_template('index.html', href2='')
