674bcd1522a74a6f31c702a69154c34715c8217f
MELSElearn <78197595+MELSElearn@users.noreply.github.com>
2022-03-31 12:41:48 +0800

    Update hands_new2.js

--- app/static/hands_new2.js
+++ app/static/hands_new2.js
@@ -1,6 +1,9 @@
 const videoElement = document.getElementsByClassName('input_video')[0];
 const canvasElement = document.getElementsByClassName('output_canvas')[0];
 const canvasCtx = canvasElement.getContext('2d');
+videoElement.setAttribute('autoplay', '');
+videoElement.setAttribute('muted', '');
+videoElement.setAttribute('playsinline', '');
 var x,y,width, height;
 x = 100;
 y = 100;
